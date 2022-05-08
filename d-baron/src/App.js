173158111708b5a6960cd35e4b0dfb4a45e0204f
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-05-08 14:13:50 -0400

    basic layout complete

---
+++ d-baron/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="first-row flex-row d-flex justify-content-center  App">
+      <div className="first-column d-flex flex-column col-12 justify-content-between">
+        <div className="header-div d-flex p-2">Box 1</div>
+        <div className="body-div d-flex">Box 2</div>
+        <div className="footer-div d-flex p-2">Box 3</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
