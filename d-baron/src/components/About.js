07bbc40f42079b482b75c9a2450324a0a3e05fd9
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-05-09 13:26:02 -0400

    mobile first layout for About me complete

--- d-baron/src/components/About.js
+++ d-baron/src/components/About.js
@@ -4,17 +4,32 @@ import React from 'react'
 
 function About() {
     return (
-        <div>
-            <br/>
-            <h5>A little about me:</h5>
-            <text>
-                I enjoy cooking and carpentry, astrophysics and the humanities.  My first love, Coffee and I, have an open relationship at times.  Listening to Jazz, binging on The Mandalorian or The Expanse or walking my dogs are great ways to spend some time.
-            </text>
-            <br/>
-            <br/>
-            <text>You can find me on GitHub at: </text><a href='https://github.com/H0RSESH0E'>H0RSESH0E</a>
-            <br/>
-            <img src="https://github.com/H0RSESH0E.png" alt="user avatar" width="95" />
+        <div className="container">
+            <div className="row d-flex justify-content-center">
+                <div className="col-12 d-flex justify-content-center align-items-center">
+                    <div>A little about me:</div>
+                </div>
+            </div>
+            <div className="row d-flex justify-content-center">
+                <div className="col-12 col-md-3 d-flex justify-content-center align-items-center">
+                    <img src="https://github.com/H0RSESH0E.png" alt="user avatar" />
+                </div>
+                <div className="col-12 col-md-9 d-flex align-items-center">
+
+                    <text>
+                        I enjoy cooking and carpentry, astrophysics and the humanities.  My first love, Coffee and I, have an open relationship at times.  Listening to Jazz, binging on The Mandalorian or The Expanse or walking my dogs are great ways to spend some time.
+                    </text>
+                </div>
+
+            </div>
+            <br></br>
+            <div className="row d-flex justify-content-center">
+                <div className="col-12 d-flex justify-content-center">
+
+                    <text>You can find me on GitHub at:&nbsp;</text><a href='https://github.com/H0RSESH0E'> H0RSESH0E</a>
+                </div>
+            </div>
+
         </div>
     )
 }
