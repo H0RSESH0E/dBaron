9df8eb8241be63bc6671a5d30afe2e0bd2f2a252
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-05-10 15:20:45 -0400

    final draft

--- d-baron/src/components/Projects.js
+++ d-baron/src/components/Projects.js
@@ -8,19 +8,32 @@ function Projects() {
             <div className="row justify-content-around">
                 {projectsArr.map((project, i) => (
                     <>
-                        <div className="d-flex justify-content-start flex-column align-items-center project" style={{width: '300px'}}>
-                            <h4 style={{ textAlign: 'center' }}>{project.name}</h4>
+                        <div className="d-flex justify-content-start flex-column align-items-center " style={{ width: '350px' }}>
 
-                            <img
-                                src={require(`../../public/images/Projects/Screenshots/Small/${project.screenshots[0].small[0]}`)}
-                                alt={project.name}
-                                className="project img-thumbnail"
-                                // onClick={() => toggleModal(project, i)}
-                                key={project.name}
-                            />
-                            <p>{project.description}</p>
+                            <div className="row d-flex justify-content-around">
+                                <div className="col-9">
+                                    <h5 style={{ textAlign: 'start', width: '150px' }}>{project.name}</h5>
+                                </div>
+                                <div className="col-3 d-flex justify-content-center align-items-center">
+                                    <a href={project.readmeLink} title="See the repo">
+                                        <h5><i class="fa fa-github-alt" aria-hidden="true"></i></h5>
+                                    </a>
+                                </div>
+                            </div>
 
+                            <a href={project.deploymentLink} title="See the deployment">
+                                <img
+                                    src={require(`../../public/images/Projects/Screenshots/Small/${project.screenshots[0].small[0]}`)}
+                                    alt={project.name}
+                                    className="project img-thumbnail"
+                                    // onClick={() => toggleModal(project, i)}
+                                    key={project.name}
+                                />
 
+                            </a>
+                            <div  style={{ width: '200px' }}>
+                                <p style={{ textAlign: 'center' }} >{project.description}</p>
+                            </div>
                         </div>
                     </>
                 ))}
--- d-baron/src/components/Resume.js
+++ d-baron/src/components/Resume.js
@@ -5,14 +5,14 @@ function Resume() {
         <>
             <section className="main-container container">
                 <div className="row d-flex justify-content-center align-items-center">
-                    <div className="d-flex flex-column  justify-content-center">
+                    <div className="d-flex flex-column justify-content-center" style={{ maxWidth: '850px' }}>
                         <h4 style={{ textAlign: 'center' }}>Here is a quick overview of skills I bring to web development roles:</h4>
                         <br></br>
-                        <p>Front-End: HTML5, CSS, Bootstrap, JavaScript, jQuery, AJAX, ReactJS, Handlebars.js</p>
-                        <p>Back-End: Node.js, Express.js, MySQL, Sequelize ORM, MongoDB, Mongoose ODM, Insomnia</p>
-                        <p>Concepts: APIs, REST, MVC, TDD, JSON, XML</p>
-                        <p>Tools: Git, Github, GitLab, Firebase, Heroku, JawsDB, VS Code5</p>
-                        <p>Certifications: Site Master Line Sweep Certification - Anritsu, The Art of Encouragement - Ontario Society of Adlerian Psychology</p>
+                        <p><b>Front-End:</b> HTML5, CSS, Bootstrap, JavaScript, jQuery, AJAX, ReactJS, Handlebars.js</p>
+                        <p><b>Back-End:</b> Node.js, Express.js, MySQL, Sequelize ORM, MongoDB, Mongoose ODM, Insomnia</p>
+                        <p><b>Concepts:</b> APIs, REST, MVC, TDD, JSON, XML</p>
+                        <p><b>Tools:</b> Git, Github, GitLab, Firebase, Heroku, JawsDB, VS Code5</p>
+                        <p><b>Certifications:</b> Site Master Line Sweep Certification - Anritsu, The Art of Encouragement - Ontario Society of Adlerian Psychology</p>
                     </div>
                     <div className="d-flex justify-content-center ">
                         <h5 style={{ textAlign: 'center' }}>Please download my whole web development CV here:</h5>
