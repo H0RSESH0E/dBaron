bf44d2703ea157025b3222e123cdcb84afbc85af
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-05-24 16:57:05 -0400

    solving link issue on build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const divStyle = {
 function Footer() {
     return (
         <footer className="container">
-            <div className="row justify-content-center">
+            <div className="row justify-content-center"> 
                 <div className="col-12 d-flex justify-content-around">
                         <a href="https://github.com/H0RSESH0E" rel="noopener noreferrer" target="_blank"><i class="fa fa-github" aria-hidden="true"></i> GitHub</a>
                         <a href="www.linkedin.com/in/david-baron-web-developer" rel="noopener noreferrer" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i> LinkedIn</a>
